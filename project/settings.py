ad0c49099ea1d240e063cfbd727250dd045ff76c
yesdopepe <laiskashkash33@gmail.com>
2025-05-07 16:36:02 +0300

    changed the styling to use bootstrap feat: Posts are shown as a bento box

--- project/settings.py
+++ project/settings.py
@@ -117,6 +117,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'posts/static',
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
